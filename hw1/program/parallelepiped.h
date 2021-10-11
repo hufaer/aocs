36803446b3e63af1a588ba6cd1e72aa7edcff4cd
hufaer <71384597+hufaer@users.noreply.github.com>
2021-10-11 20:33:38 +0400

    Create parallelepiped.h

---
+++ hw1/program/parallelepiped.h
@@ -0,0 +1,28 @@
+#ifndef __parallelepiped__
+#define __parallelepiped__
+
+//------------------------------------------------------------------------------
+// parallelepiped.h - содержит описание параллелепипеда
+//------------------------------------------------------------------------------
+
+#include "stdio.h"
+#include "random.h"
+
+// параллелепипед
+struct parallelepiped {
+    int a, b, c; // ребра
+};
+
+// Ввод параметров параллелепипеда из файла
+void In(parallelepiped& p, FILE* file);
+
+// Случайный ввод параметров параллелепипеда
+void InRnd(parallelepiped& p);
+
+// Вывод параметров параллелепипеда в форматируемый поток
+void Out(parallelepiped& p, FILE* file);
+
+// Вычисление длины параллелепипеда
+double SurfaceArea(parallelepiped& p);
+
+#endif //__parallelepiped__
